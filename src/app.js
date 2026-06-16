f910be50bdca5b1225014c57afda27235dd12d8b
SapphicBear <sleepyscreenart@gmail.com>
2026-06-16 18:59:28 +0200

    add error handling default

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT | 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.sendStatus(err.statusCode || 500);
+});
+
 app.listen(PORT, (error) => {
     if (error) {
         throw error;
