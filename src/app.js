892c94cceaea2fd0d204089d9bfce7b68b216343
SapphicBear <sleepyscreenart@gmail.com>
2026-06-17 17:58:58 +0200

    fix whitespace error

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(`${models.uri.DEFAULT_URI}${models.uri.URI_ENUM[0]}`, routes.users);
 app.use(`${models.uri.DEFAULT_URI}${models.uri.URI_ENUM[1]}`, routes.posts);
+
 app.use((err, req, res, next) => {
     console.error(err);
     res.sendStatus(err.statusCode || 500);
