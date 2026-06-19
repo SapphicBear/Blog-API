f7a98fa0b471c08d0fb38158a8b04fc0914bad23
SapphicBear <sleepyscreenart@gmail.com>
2026-06-19 16:58:52 +0200

    Wrap async functions in try/catch blocks
    Make all prisma functions wrapped in try/catch blocks to
    propogate errors further on.

--- src/controlers/posts.js
+++ src/controlers/posts.js
@@ -4,29 +4,49 @@ const controller = {
     async get(req, res) {
         // If param/query wants all, send all, otherwise, return only post specified by id
         if (!req.params.postId) {
-            req.posts = await prisma.post.findMany();
+            try {
+                req.posts = await prisma.post.findMany({
+                    include: {
+                        comments: true,
+                        author: true,
+                    },
+                });
+                res.json(req.posts);
+            } catch (err) {
+                console.error(err);
+                res.sendStatus(400).json(err);
+            }
         } else {
             try {
                 req.posts = await prisma.post.findUnique({
+                    include: {
+                        comments: true,
+                    },
                     where: {
                         id: parseInt(req.params.postId),
                     },
                 });
+                res.json({ posts: req.posts });
             } catch (err) {
                 console.error(err);
+                res.json(err);
             }
         }
-        res.json({ posts: req.posts });
     },
     async post(req, res) {
-        const post = {
-            title: req.query.title,
-            content: req.query.content,
-            author: req.query.author,
-            authorId: req.query.authorId,
-        };
-        req.posts = [post];
-        res.json({ posts: req.posts });
+        try {
+            await prisma.post.create({
+                data: {
+                    title: req.query.title,
+                    content: req.query.content,
+                    authorId: parseInt(req.query.authorId),
+                },
+            });
+            res.json({ msg: "Post added to the database" });
+        } catch (err) {
+            console.error(err);
+            res.json({ error: err });
+        }
     },
 };
 
--- src/controlers/users.js
+++ src/controlers/users.js
@@ -3,43 +3,54 @@ import { prisma } from "./../../lib/prisma.js";
 const controller = {
     async get(req, res) {
         if (!req.params.userId) {
-            req.users = await prisma.user.findMany({
-                include: {
-                    posts: true,
-                    comments: true,
-                },
-            });
+            try {
+                req.users = await prisma.user.findMany({
+                    include: {
+                        posts: true,
+                        comments: true,
+                    },
+                });
+            } catch (err) {
+                console.error(err);
+                res.json(err);
+            }
         } else {
-            req.users = await prisma.user.findUnique({
-                where: {
-                    id: parseInt(req.params.userId),
-                },
-                include: {
-                    posts: true,
-                    comments: true,
-                },
-            });
+            try {
+                req.users = await prisma.user.findUnique({
+                    where: {
+                        id: parseInt(req.params.userId),
+                    },
+                    include: {
+                        posts: true,
+                        comments: true,
+                    },
+                });
+                res.json({ users: req.users });
+            } catch (err) {
+                console.error(err);
+                res.json(err);
+            }
         }
-        res.json({ users: req.users });
     },
     async post(req, res) {
         // A user has a username, email, posts and comments
-        req.users = {
-            name: req.query.name,
-            email: req.query.email,
-            role: req.query.role,
-        };
-        await prisma.user.create({
-            include: {
-                posts: [],
-                comments: [],
-            },
-            data: {
-                email: req.query.email,
-                name: req.query.name,
-            },
-        });
-        res.json({ msg: "User added to database" });
+        try {
+            await prisma.user.create({
+                include: {
+                    posts: [],
+                    comments: [],
+                },
+                data: {
+                    email: req.query.email,
+                    name: req.query.name,
+                    password: req.query.password,
+                },
+            });
+            res.json({ msg: "User added to database" });
+        } catch (err) {
+            console.error(err);
+            res.sendStatus(400).json({ error: err });
+        }
     },
 };
 
