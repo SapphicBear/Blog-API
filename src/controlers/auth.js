998247641cf4783b89f0b93e5ac379a8fde38061
SapphicBear <sleepyscreenart@gmail.com>
2026-07-09 20:52:02 +0200

    fix validation for login

--- src/controlers/auth.js
+++ src/controlers/auth.js
@@ -7,35 +7,34 @@ const controller = {
     login: [
         validation.login,
         (req, res, next) => {
+            let err;
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
-                req.errors = errors.array();
-                res.status(400);
-            } else {
-                req.body = matchedData(req);
-                passport.authenticate(
-                    "local",
-                    { session: false },
-                    (err, user, info) => {
-                        if (err || !user) {
-                            return res.status(400).json({
-                                message: info
-                                    ? info.message
-                                    : "Something is not right",
-                                user: user,
-                            });
-                        }
-                        req.login(user, { session: false }, (err) => {
-                            if (err) {
-                                res.send(err);
-                            }
-                            // generate a signed json web token
-                            const token = jwt.sign(user, process.env.SECRET);
-                            return res.json({ user, token });
+                err = errors.array();
+            }
+            req.body = matchedData(req);
+            passport.authenticate(
+                "local",
+                { session: false },
+                (err, user, info) => {
+                    if (err || !user) {
+                        return res.status(400).json({
+                            message: info
+                                ? info.message
+                                : "Something is not right",
+                            user: user,
                         });
                     }
-                )(req, res);
-            }
+                    req.login(user, { session: false }, (err) => {
+                        if (err) {
+                            res.send(err);
+                        }
+                        // generate a signed json web token
+                        const token = jwt.sign(user, process.env.SECRET);
+                        return res.json({ user, token });
+                    });
+                }
+            )(req, res);
         },
     ],
 };
